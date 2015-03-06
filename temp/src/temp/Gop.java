1c2c13ea52f23e1cb7e3cc49d18cf9e439bbed03
LeeYunJae <bliss0410@hanmail.net>
2015-03-06 14:34:39 +0900

    ..

---
+++ temp/src/temp/Gop.java
@@ -0,0 +1,35 @@
+package temp;
+
+public class Gop {
+
+	/*
+		long result = 1 ;
+		
+		for(int i=1 ; i<=100; i++) {
+			result *= i ;
+		}
+		
+		System.out.println("result = " + result);
+		
+		System.out.println("======================");
+		
+		for(int i=1 ; i<11 ; i++) {
+			System.out.println("i = " + i);
+		}
+	;*/
+		
+	public void gugudan(){
+		
+		System.out.println("구구단을 외자 구구단을 외자");
+		
+		for(int i=2 ; i<10 ; i++) {
+			for(int j=1 ; j<10 ; j++) {
+				System.out.print(i + "*" + j + "=" + i*j + "\t");
+			}
+			System.out.println();
+		}
+	}
+		
+		
+}
+
---
+++ temp/src/temp/Human.java
@@ -0,0 +1,13 @@
+package temp;
+
+public class Human {
+
+	public static void main(String[] args) {
+		
+		Gop gop = new Gop() ;		
+		gop.gugudan(); 
+		
+
+	}
+
+}
