7c5f20c3e6f86f493b3f633daa055c3c99adf404
LeeYunJae <bliss0410@hanmail.net>
2015-03-11 16:53:19 +0900

    bigbang

---
+++ temp/src/daejeon/design/calculator/One.java
@@ -0,0 +1,22 @@
+package daejeon.design.calculator;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+
+public class One implements ActionListener{
+	JTextField text ;
+	
+	public One(JTextField text) {
+		// TODO Auto-generated constructor stub
+		this.text = text ;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		text.setText(text.getText()+"1") ;
+		//text.setText(text.getText()+1) ; 문자+숫쟈=>문자가 되므로 써도 에러는x	
+	}
+
+}
